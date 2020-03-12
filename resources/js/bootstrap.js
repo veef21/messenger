aeba83d20cdf3c34389d67125dd9a2dcb1f6965c
Veef <endrielfer.v@gmail.com>
2020-03-12 12:50:48 -0400

    Login UI advances

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,13 @@
-window._ = require('lodash');
+//window._ = require('lodash');
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
  * code may be modified to fit the specific needs of your application.
  */
-
+/*
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
@@ -13,6 +15,7 @@ try {
     require('bootstrap');
 } catch (e) {}
 
+*/
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<b-container>
+    <b-row align-h="center">
+        <b-col cols="8">
+            <b-card title="Inicio de Sesión" header-tag="header">
+                <b-alert show>
+                    Por favor ingrese sus datos:
+                </b-alert> 
+                <b-form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <b-form-group label="Usuario o correo electrónico:" label-for="email">
+                        <b-form-input type="email"
+                            id="email" name="email"
+                            value="{{ old('email') }}" required autocomplete="email" autofocus
+                            placeholder="Correo@dominio.com">
+                        </b-form-input>
+                    </b-form-group>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    <b-form-group label="Contraseña:" label-for="password">
+                        <b-form-input type="password"
+                            id="password" name="password" required autocomplete="password"
+                            placeholder="********">
+                        </b-form-input>
+                    </b-form-group>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <b-form-group>
+                        <b-form-checkbox name="remember" {{ old('remember') ? 'checked="true"' : '' }}>
+                            Recordar Sesión
+                        </b-form-checkbox>
+                    </b-form-group>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <b-button type="submit" variant="primary">
+                        Ingresar
+                    </b-button>
+                    <b-button href="{{ route('password.request') }}" variant="link">
+                        ¿Olvidaste tu contraseña?
+                    </b-button>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                </b-form>
+            </b-card>
+        </b-col>
+    </b-row>
+</b-container>
 @endsection
